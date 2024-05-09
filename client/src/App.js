a345baa7fb17d75eef5243a460cb5b7a19df08e8
alprival <alexprivalov@outlook.fr>
2024-05-09 15:48:37 +0200

    hot reload react

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,11 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <h2>Welcome to React</h2>
+          <h2>Welcome to React</h2>
+          <h2>Welcome to React</h2>
+          <h2>Welcome to React</h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
